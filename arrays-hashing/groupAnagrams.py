b389f4de41c4f56e485a89a4b81006c9e61842c9
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-08-23 13:49:20 -0300

    better understand....

--- arrays-hashing/groupAnagrams.py
+++ arrays-hashing/groupAnagrams.py
@@ -8,29 +8,19 @@ def groupAnagrams(strs):
     # O(n)
 
     # we are saving what chars the string has equal each other and how many of them too.
+    # we start with List <type> because, tuple does not have 'append()' method
     ans = defaultdict(list)
 
     count = []
     for s in strs:
         count = [0] * 26  # a ... z
         for c in s:
+            # checking for same index ... [ord('e') - ord('a')] will be an index of count, so we'll change the actual value, adding 1
             count[ord(c) - ord('a')] += 1
             # list are unhashable, cannot be 'key', 'cause they're mutable
         ans[tuple(count)].append(s)
 
     print(ans.values())
 
-    # O(mnlogn)
-    # m -> strs' length
-    #  nlogn -> sorted() method
-    # hashmap = {}
-    # for st in strs:
-    #     key = ''.join(sorted(st))
-    #     if key not in hashmap:
-    #         hashmap[key] = [st]
-    #     else:
-    #         hashmap[key] += [st]
-    # return hashmap.values()
 
-
-print(groupAnagrams(["eae", "aea", "tan", "ate", "nat", "bat"]))
+print(groupAnagrams(["tae", "eat", "tan", "ate", "nat", "bat"]))
