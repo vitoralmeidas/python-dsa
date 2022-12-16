2f19f927dddebd523fe0d96f9e396849b3ec13f5
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-12-15 22:42:53 -0300

    Bucket Sort Algo

--- arrays-hashing/topKFrequent.py
+++ arrays-hashing/topKFrequent.py
@@ -1,16 +1,24 @@
 def topKFrequent(nums, k):
-    num_dic = {}
+    count = {}
+    for n in nums:
+        count[n] = 1 + count.get(n, 0)
+
+    freq = [[] for i in range(len(nums) + 1)]
+
+    for v, i in count.items():
+        # i count, v actual numbers in nums
+        freq[i].append(v)
+
     ans = []
-    for num in nums:
-        if num in num_dic:
-            num_dic[num] += 1
-        else:
-            num_dic[num] = 1
+    # backwards loop, because the tops are the last ones
+    # range(last_elemenet, until_index, loop_order)
+    for j in range(len(freq) - 1, 0, -1):
+        # checking all elements in the inner arrays
+        for i in freq[j]:
+            ans.append(i)
+            # make sure that the result will stop when we already have the K-Tops
+            if len(ans) == k:
+                return ans
 
-    # sorting..
-    sorted_num_dic = sorted(num_dic.items(), key=lambda x: x[1], reverse=True)
 
-    return print([x[0] for x in sorted_num_dic[:k]])
-
-
-topKFrequent([1, 1, 1, 2, 3, 2], 2)
+print(topKFrequent([1, 1, 1, 2, 3, 2], 2))
